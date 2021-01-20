afd1d1401da1499d3885c80da197f5b6c22fe232
ModestGoblin <sikmat@mcmaster.ca>
2021-01-20 08:28:41 -0500

    Added project
    with necessary capabilites

---
+++ Citadel/Shared/CitadelApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitadelApp.swift
+//  Shared
+//
+//  Created by Thomas Sikma on 2021-01-20.
+//
+
+import SwiftUI
+
+@main
+struct CitadelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Citadel/Shared/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Thomas Sikma on 2021-01-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, home!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
